4a3d7e3d33568cdd9e42494fdfd70980621c1b55
Surya Varma <suryavarma919@gamil.com>
2023-05-25 14:13:45 +0530

    outline has completed

---
+++ docs/script.js
@@ -0,0 +1,285 @@
+let data=[
+    {
+        "title":"First C Program",
+        "link":"https://www.youtube.com/embed/LNz1ULDrk9c",
+        "description":`First video
+        00:00:00 to 00:00:46- about games description briefly
+        00:02:23 to 00:12:09-  Self introduction about trainers
+        00:15:15 -to  00:18:20 -  Discuss some games
+        00:20:55 to 00:31:05 - Doubts solving session
+        00:35:17 to 00:38:00 - Unit 3 lesson 1 and lesson 2 coding description
+        00:39:36 - 00:43:29 -Unit 3 lesson 3,4,5 coding description
+        00:45:51 to 00:46:18 - Unit3 refrence code<br>00:46:24 to 00:49:34- Discuss game command icons
+        01:05:18 to 01:09:41- Framework of  c programming and context of Repl
+        01:11:00 to  01:13:33- Explain the Icons in the Code
+        01:14:00 to 01:17:54 - Explain the  instructions and how to write the code  in the patterns
+        01:20:31 to 01:22:35 - Doubts solving session<br>01:28:47 to 01:30:28-How to reach out to clarify Doubts`
+    },
+    {
+        "title":"C Programming 1_1",
+        "link":"https://www.youtube.com/embed/OeDoWnQ17z4",
+        "description":`00:09:42 to 00:15:06 - Briefly revise previous topics
+        00:18:43 to 00:25:52 - Explain pattern 1 with two commands in faculty view
+        00:27:22 to 00:30:25 - Explain how to type ,test the pattern1 in faculty view
+        00:30:51 to 00:33:26 - Overview of 7 steps to follow in program in students view<br>00:33:43 to 00:38:12 - Detailed explanation of 7 steps
+        00:49:25 to 00:50:58 - Explain some common errors in the code
+        00:56:21 to 00:58:30 - Trainer check the students code
+        01:08:29 to 01:20:19 - Doubts solving session
+        01:36:00 to 01:38:05 - Briefly explain new command in sample program
+        01:38:25 to 01:39:18 - Assign a task<br>01:52:49 to 01:56:20- Doubts Solving Session
+        02:03:35 to 02:05:20 - Doubts solving session<br>02:06:22 to 02:07:43 - Summary of today's class`
+    },
+    {
+        "title":"C Programming 2",
+        "link":"https://www.youtube.com/embed/QJ3gMw079HQ",
+        "description":`
+        00:05:00 to 00:06:54 -  Given check list to follow daily
+        00:06:55 to 00:09;20 -Explanation of au.e42.dev website with Game 2048
+        00:10:21 to 00:13:05 -  Discuss Karel game 
+        00:13:20 to 00:15:16 - Given order to follow in the check list 
+        00:16:10 to 00:18:34 - Summarise the previous class
+        00:19:20 to 00:31:30 - Explanation of new commands in pattern4
+        01:05:27 to 01:06:28 -Solve some common errors in coding
+        01:09:14 to 01:11:48- Doubts solving session
+        01:13:04 to 01:15:21 - Clear the doubts in pattern 4
+        01:15:28 to 01:19:00- Explain the pattern 5
+        01:19:38 to 01:19:53 -  Given some hints while writing code
+        01:23:15 to 01:39:00 - Doubts solving session about Game Karel
+        `
+    },
+    {
+        "title":"C Programming 4",
+        "link":"https://www.youtube.com/embed/oNC_k-NBxvM",
+        "description":`00:01:20 to 00:13:28 - Overview of previous classes topics 
+        00:14:17 to 00:18:38 - What is programming Language
+        00:23:57 to 00:31:05 - Discuss pattern 6 and errors
+        00:46:43 to 00:48:20- What is fatal error
+        00:48:40 to 00:51:43 - Experimenting some errors in pattern 7
+        01:08:09 to 01:12:18 - Discuss pattern 7.1
+        01:13:11 to 01:20:35 - Discuss pattern 8
+        01:21:41 to 01:26:20 - Steps for new commands in pattern8
+        01:30:40 to 01:32:45 - Explain New commands in pattern 9 
+        01:34:11 to 01:36:38-  Assign pattern9 task
+        01:37:54 to 01:40:20 - Explain console,shell and instructions and  Assign task in pattern 10`
+    },
+    {
+        "title":"C Programming 6",
+        "link":"https://www.youtube.com/embed/zE_xsK7ZaEc",
+        "description":`00:01:02 to 00:03:18- Agenda for what we are going to discuss 
+        00:03:25 to 00:14:20 - Slideshow of previous classes
+        00:21:20 to 00:34:00   - Clear doubts in aksharaabhyasam 
+        00:34:40 to 00:42:55 - Clear doubts in pattern 8 
+        00:50:54 to 00:55:54-  Clear linked command error doubt in patter 8 
+        01:04:55 to 01:12:28- Clear  doubts in pattern 9  
+        01:16:25 to 01:36:56  - Clear doubts in pattern 10 and discuss segmentation error ,predefined & defined,New commands.
+        01:40:30 to 01:43:15 - clear doubts in pattern 9
+        01:43:55 to 01:45:10 - Remove make good command in pattern10 
+        01:53:00 to 01:54:45 - Solving doubts`
+    },
+    {
+        "title":"C Programming 7",
+        "link":"https://www.youtube.com/embed/sbOT4cgIX54",
+        "description":`00:06:21 to 00:12:06 -Slideshow of previous class
+        00:26:21 to 00:39:25 - Explain what is declaration,use,value,assign value,variable,type of data and name in the code ?
+        00:39:50 to 00:40:35 - Assign a task in pattern 11 using variable
+        00:48:10 to 00:49:38 - Doubts solving session
+        00:50:39 to 00:57:15 -  Doubts solving session
+        01:01:53 to 01:02:55 - Doubts solving session
+        01:05:21 to 01:06:51 - Explain Variable pattern 12
+        01:08:04 to 01:18:35 - Doubts solving in pattern 12 &Explain Operation
+        01:28:01 to 01:33:30 - Discuss C language vocabulary
+        01:42:17 to 01:51:35- Clear doubts while explain pattern 12 with the changed website &Assign a task  and Explain operators (Addition, Subtraction, Multiplication, Division,Modulo division)
+        01:52:05 to 01:52:54 - Show the command to open the webpage in the website 
+        01:53:00 to 01:56:20 - Summary of Covered topics and Cleared doubt in webage`
+    },
+    {
+        "title":"C Programming 8",
+        "link":"https://www.youtube.com/embed/V1ygs7jr4Zs",
+        "description":`00:03:20 to 00:08:05 - For new joining orientation 
+        00:09:56 to 00:22:58 - Summary of previous class
+        00:31:55 to 00:36:12 - Discuss New  Functions in pattern 10
+        00:37:16 to 00:45:00 - Explain control of over flow in demo program
+        00:45:45 to  01:02:10 - Doubts solving session
+        01:02:11 to 01:09:47 - Discuss two variables in sum of numbers program
+        01:09:55 to 01:14:14 - Explain sum of two numbers program
+        01:17:15 to 01:18:29 - Solving Doubts
+        01:19:58 to 01:20:50 - Discuss New variable and assign the result in sum of two numbers program 
+        01:24:41 to 01:25:39 - Clear doubt about double quotes
+        01:26:41 to 01:27:12 - Explain two commands that we don't use  in sum of two numbers program`
+    },
+    {
+        "title":"C Programming 9",
+        "link":"https://www.youtube.com/embed/ONa2TaoqJ1o",
+        "description":`00:01:34 to 00:05:50 - Summary of previous class
+        00:05:57 to 00:07:07 - Creating new questions 
+        00:08:07 to 00:12:12 - Explain 3by 3 matrix program
+        00:12:45 to 00:15:15 - Explain the importance of reflection after completion of activity & Explain option to see the history in the code
+        00:16:00 to 00:17:38 - Show the demo part in faculty view
+        00:18:45 to 00:31:33 - Check the students coding and clear their errors as an example to explain reflection.
+        00:45:02 to 00:47:15 - Assign a task and signup brilliant.org
+        00:50:11 to 00:50:54 - Listed a  websites that are using in c programming
+        00:51:14 to 00:51:51 - Doubt about brilliant.org
+        00:58:27 to 01:01:04 - Explain pattern 17
+        01:01:58 to 01:03:45 - Explain pattern 18
+        01:05:20 to 01:06:43 - Cleared doubts
+        01:28:04 to 01:29:06 - Create rooms to what will do in tomorrow class
+        01:51:49 to 01:52:03 - Create a pole and asking to take the pole`
+    },
+    {
+        "title":"C Programming 11",
+        "link":"https://www.youtube.com/embed/3W7jGCYHEO8",
+        "description":`00:16:30 to 00:22:10 - Explain functions vocabulary in geometric expression program
+        00:24:30 to 00:27:24 - To find functions in sum of two digit numbers
+        00:30:00 to 00:40:09 - Explain few collection of rules in demo sum of two digits program
+        00:42:21 to 00:46:55 - Doubts solving session
+        00:47:51 to 00:51:50 - Explain the use of comments in the code 
+        00:53:43 to 01:00:10 - Explain return data type in demo sum of two digits numbers
+        01:02:26 to 01:08.55 - Doubts solving session
+        01:09:36 to 01:10:55 - Assign control flow program to try
+        01:16:56 to 01:21:30 - Explain control flow , expression and evaluation in control flow program
+        01:24:23 to 01:26:08 - Doubt solving session
+        01:26:45 to 01:36:18 - Discuss Reverse of two numbers and Cleared doubts in that program
+        01:36:55 to 01:40:00  - Breif demo of what we discuss tommorrow
+        01:42:29 to 01:43:34 - Assign a  challenge in brilliant.org`
+    },
+    {
+        "title":"C Programming 12_1",
+        "link":"https://www.youtube.com/embed/AZrqebSs-9Q",
+        "description":`00:03:10 to 00:07:05 - Assign a functions task in reverse of two numbers
+        00:07:35 to 00:08:16 - Explain type reverse of number
+        00:08:58 to 00:11:16 - Explain sum of squares of numbers
+        00:12:10 to 00:17:05 - Assign challenge in brilliant.org`
+    },
+    {
+        "title":"C Programming 12_3",
+        "link":"https://www.youtube.com/embed/2KhUDSjgya4",
+        "description":`00:02:16 to 00:06:11 - Context of what future me?
+        00:06:20 to 00:14:49 - Concept of start and stop in coding and make an example in Karel
+        00:15:04 to 00:18:28 - Explain example in concepts start and stop in coding
+        00:19:24 to 00:21:22 - Translate the functions in the code 
+        00:22:21 to 00:24:20 - Clear doubts
+        00:24:53 to 00:27:08 - Discuss function calling from start not from stop.
+        00:27:20 to 00:28:00 - Assign task in control flow
+        00:28:43 to 00:29:45 - Summarize the context of start and stop
+        00:30:57 to 00:33:43   - Make a change by using three words as synonyms
+        00:38:54 to 00:45:18 - Discuss the topic of different input and different output with examples 
+        00:49:39 to 00:52:58 -Summary of today's class`
+    },
+    {
+        "title":"C Programming 13",
+        "link":"https://www.youtube.com/embed/sgqT5wU9RWw",
+        "description":`00:02:10 to 00:15:38 - Preview of previous class
+        00:16:14 to 00:22:10 - Doubts solving session
+        00:22:44 to 00:25:10 - Discuss some important topic that we should Mark down in demo code &Explain another datatype
+        00:25:20 to 00:35:53 -Explain  functions in  triangle pattern
+        00:36:42 to 00:44:42 - Explain Format in different ways
+        00:45:11 to 00:58:27 - Explain the bill format & function  use printf.   instead  demo code
+        01:01:27 to 01:09:11- Cleared doubts
+        01:09:42 to 01:18:40 - Explain format of functions Right and left alignment,width,printf with an example
+        01:18:48 to 01:20:20 - Cleared doubts
+        01:21:30 to 01:25:14 -Explain function version 3 in demo code
+        01:31:16 to 01:32:10 - Explain special meaning of percentage in function type the enter key in demo code
+        01:32:37 to 01:36:38 - Explain tabspace
+        01:37:03 to 01:38:25 - Cleared doubts
+        01:39:03 to 01:44:50- Explain undefined reference and \n in printf function
+        01:45:16 to 01:50:30- Cleared doubts and explain double percentage use and double backward slash
+        01:53:23 to 01:54:27 - Explain printf use
+        01:56:50 to 01:58:00 - Explain difference between void start and int main
+        01:59:00 to 02:00:10 - Brief introduction of fundamentals of computing`
+    },
+    {
+        "title":"C Programming 14",
+        "link":"https://www.youtube.com/embed/MWFVw95zMKM",
+        "description":`00:01:10 to 00:02:28 - How to open internal commands in website 
+        00:26:31 to 00:28:10 - Discuss new material
+        00:29:10 to 00:38:30-Explain what is computer and printed circuit board 00:41:00 to 00:48:08- Explain PCB,functions,binary format,execution
+        00:48:25 to 00:49:48 - Cleared doubt
+        00:50:15 to 00:52:43 - Explain theory of sample program
+        00:53:07 to 00:54:21 - Cleared doubts 
+        00:55:05 to 00:56:50 - Cleared doubt about memory location
+        00:57:31 to 01:04:14 - Cleared doubts about OS bits
+        01:04:50 to 01:10:35- Cleared doubts about speed 
+        01:10:55 to 01:25:30 -  Explain execution &cleared doubts
+        01:25:35 to  01:39:39 - Explain assembly language and Cleared doubts
+        01:39:42 to 01:43:07 - Explain input ,output and operating system
+        01:43:13 to 01:53:45 - Explain need for a better language and compiler with an example
+        01:55:00 to 02:00:45 - Cleared doubts &assign task in brilliant.org`     
+    },
+    {
+        "title":"C Programming 15",
+        "link":"https://www.youtube.com/embed/Ez5eyWKzyYQ",
+        "description":`00:01:50 to 00:04:03 - Brief introduction of what we discuss today
+        00:07:00 to 00:09:06 - Discuss  right align in printf demo program
+        00:09:28 to 00:15:00 - Explain special characters in printf demo code
+        00:15:32 to 00:16:59 - Explain example of printf
+        00:20:25 to 00:23:24 - Explain why we need fundamentals
+        00:23:32 to 00:31:36 - Review of fundamentals of computing
+        00:32:34 to 00:34:12 - Explain website for new joiners
+        00:34:40 to 00:51:47 - Cleared doubts
+        00:53:51 to 00:57:42 - Cleared doubt about run time error and compile error
+        00:59:00 to 01:00:49 - Discuss compile error with an example
+        01:02:23 to 01:03:45 - Explain building blocks in brilliant.org
+        01:06:48 to 01:08:30- Given example of runtime and compile errors
+        01:09:03 to 01:24:40 - Explain idhi ayithe and idhi kakapothey with an example
+        01:27:28 to 01:29:30 - Explain block of code in c vocabulary
+        01:31:02 to 01:49:50 - Explain how to print first n natural numbers in code 
+        01:50:27 to 02:11:53 - Cleared doubts and explain get int prompt use`
+    },
+    {
+        "title":"C Programming 17",
+        "link":"https://www.youtube.com/embed/Sc6RFm6WZdk",
+        "description":`00:05:33 to 00:08:30 - Context of string literals&integer literal 
+        00:08:46 to 00:12:45 - Briefly discuss get int
+        00:18:12 to 00:27:44 - Explain get int function,random value &junk value
+        00:28:09 to 00:52:50 - Explain  Integrated development environment & keyboard shortcuts with example and Cleared doubts
+        00:53:01 to 00:58:00 - Explain IDE in GitHub website
+        01:00:00 to 01:03:31 - Explain get string and format specifiers 
+        01:03:50 to 01:07:40 - Cleared doubts 
+        01:11:39 to 01:23:30 - Explain conditions in code
+        01:25:05 to 01:29:19 - Cleared doubts
+        01:29:36 to 01:31:35 - Cleared doubts about if else
+        01:32:21 to 01:35:48 - Explain subscript in code
+        01:35:50 to 01:59:10 - Explain Arrays,Matrix and while loop with example 
+        01:59:50 to 02:02:58 - Brief introduction of what we discuss tomorrow`
+    },
+    {
+        "title":"C Programming 18",
+        "link":"https://www.youtube.com/embed/XnA8Nm9W-3g",
+        "description":`00:01:37 to 00:02:40 - What we discuss today
+        00:05:36 to 00:08:27 - Discuss time period for preparation
+        00:09:27 to 00:13:05 - Context of enhance42
+        00:17:28 to 00:27:04 - Slideshow of previous class
+        00:28:30 to 00:36:50 - Preview of previous topics and assign task
+        00:37:10 to 00:42:20 - Discuss arrays and given time for writing code
+        00:48:55 to 00:57:41 - Explain arrays with example
+        00:58:11 to 00:59:53 - Explain main function
+        01:00:01 to 01:14:24 - Explain main function visualisation with example array in au.e42 website `
+    },
+    {
+        "title":"C Programming 19",
+        "link":"https://www.youtube.com/embed/HhaespagBkQ",
+        "description":`00:05:54 to 00:19:03 - Slideshow of previous class
+        00:22:05 to 00:32:23 - Explain reference in Karel game
+        00:32:44 to 00:44:45 - Explain network issue
+        00:45:05 to 00:46:34 - Cleared doubts
+        00:55:00 to 01:10:19 - Explain what is 2Darray& initialisation of  4/4matrix  in Karel game 
+        01:11:04 to 01:18:14 - Cleared doubts
+        01:18:39 to 01:21:17 - Assign task in brilliant.org
+        01:21:18 to 01:28:50 - Discuss memory of program in brilliant.org
+        01:31:39 to 01:40:04 - Cleared doubts about random function
+        01:41:04 to 01:59:35 - Explain random function with example
+        02:00:14 to 02:04:01 - Cleared doubt and discuss nested function call
+        02:04:09 t0 02:06:36 - Discuss intial board with random position`   
+    }
+]
+function clicked(id) {
+    let video = data[id - 1];
+    let iframe = document.getElementById("utubevid");
+    iframe.src = video.link;
+
+    let title = document.getElementById("title");
+    title.innerHTML = video.title;
+
+    let txtarea = document.getElementById("txtarea");
+    txtarea.innerHTML = video.description;
+}
